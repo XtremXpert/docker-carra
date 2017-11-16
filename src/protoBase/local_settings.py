a1f2d566046331fb537469200194b6c64ad286e8
Benoît Vézina <benoit@vezina.biz>
2017-11-16 09:40:19 -0500

    Update local_settings.py

--- src/protoBase/local_settings.py
+++ src/protoBase/local_settings.py
@@ -25,7 +25,7 @@ DATABASES = {
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASS'],
-        'HOST': os.environ['DB_SERVICE'],
+        'HOST': os.environ['DB_HOST'],
         'PORT': os.environ['DB_PORT']
     
     }
