4cfe7cbaaea058ad799482ad386a58b9c11afd56
Benoît Vézina <benoit@vezina.biz>
2017-11-23 10:13:06 -0500

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+ #!/bin/bash
 
 # python /src/manage.py makemigrations 
 # python /src/manage.py migrate --noinput
+
+psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'my_db'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE my_db"
 
 [ ! -f /src/secret_key.txt ] && python /src/generate_key.py /src/secret_key.txt
 
